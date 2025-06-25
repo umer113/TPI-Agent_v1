8e9cf2fef48c80faa01a028252065e489a553333
Shaikh Umer Qadir <145275785+umer113@users.noreply.github.com>
2025-06-25 16:03:19 +0500

    Update chatgpt_v5.2.py

--- chatgpt_v5.2.py
+++ chatgpt_v5.2.py
@@ -450,7 +450,7 @@ def main():
     </style>
     """, unsafe_allow_html=True)
 
-    st.title("🕸️ TPI Overwatch AI")
+    st.set_page_config(layout="wide", page_title="TPI Overwatch AI")
 
     if "query" not in st.session_state:
         st.session_state["query"] = ""
